5cefb5ac353e291eea962511a9504ac61900434b
Gustav Berg <fnuf_links@hotmail.com>
2024-09-10 15:31:18 +0200

    fixat inlogg på startsida osv

--- InventoryManagementApplication/InventoryManagementApplication/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ InventoryManagementApplication/InventoryManagementApplication/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -88,20 +88,20 @@ namespace InventoryManagementApplication.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-            [DataType(DataType.Password)]
-            [Display(Name = "Password")]
+            //[Required]
+            //[StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            //[DataType(DataType.Password)]
+            //[Display(Name = "Password")]
             public string Password { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-            public string ConfirmPassword { get; set; }
+            //[DataType(DataType.Password)]
+            //[Display(Name = "Confirm password")]
+            //[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            //public string ConfirmPassword { get; set; }
 
 
             // Tillagt manuellt
@@ -156,6 +156,7 @@ namespace InventoryManagementApplication.Areas.Identity.Pages.Account
                 user.FirstName = Input.FirstName;
                 user.LastName = Input.LastName;
                 user.EmployeeNumber = Input.EmployeeNumber;
+                Input.Password = "Admin123!";
 
                 await _userStore.SetUserNameAsync(user, Input.EmployeeNumber, CancellationToken.None);
                 //await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
