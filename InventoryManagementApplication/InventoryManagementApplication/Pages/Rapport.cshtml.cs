feaf06895c548fe9a630a324b3c3717b928960fc
Elin Sand <147603404+ElinSand@users.noreply.github.com>
2024-09-11 15:16:57 +0200

    Lagt till menyalternativ

---
+++ InventoryManagementApplication/InventoryManagementApplication/Pages/Rapport.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InventoryManagementApplication.Pages
+{
+    public class RapportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
