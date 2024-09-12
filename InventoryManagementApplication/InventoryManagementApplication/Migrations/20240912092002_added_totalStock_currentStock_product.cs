5d0892b43e68297da5c31655a075c18a7be6992a
Tintin Falk <tintinfalk01@gmail.com>
2024-09-12 11:37:55 +0200

    updated Create for admin/product

---
+++ InventoryManagementApplication/InventoryManagementApplication/Migrations/20240912092002_added_totalStock_currentStock_product.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryManagementApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class added_totalStock_currentStock_product : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Stock",
+                table: "Products",
+                newName: "TotalStock");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CurrentStock",
+                table: "Products",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentStock",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "TotalStock",
+                table: "Products",
+                newName: "Stock");
+        }
+    }
+}
