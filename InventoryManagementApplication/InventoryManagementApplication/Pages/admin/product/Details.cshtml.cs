7e2278b9dc4771b47170a81446ef213d1ade1b46
Alicia Blomqvist <122750659+aliciablomqvist@users.noreply.github.com>
2024-09-24 14:44:59 +0200

    Tooltips added to required fields

--- InventoryManagementApplication/InventoryManagementApplication/Pages/admin/product/Details.cshtml.cs
+++ InventoryManagementApplication/InventoryManagementApplication/Pages/admin/product/Details.cshtml.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using InventoryManagementApplication.Data;
 using InventoryManagementApplication.Models;
 using System.Text.Json;
 using InventoryManagementApplication.DAL;
