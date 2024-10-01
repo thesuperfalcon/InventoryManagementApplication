bd36427c0a3179ba0ad445d8c7249e89e0da2b12
Gurra Berg <fnuf_links@hotmail.com>
2024-10-01 11:01:13 +0200

    fixed name collision and warning message when more then 6 storages

--- InventoryManagementApplication/InventoryManagementApplication/Pages/admin/storage/Create.cshtml.cs
+++ InventoryManagementApplication/InventoryManagementApplication/Pages/admin/storage/Create.cshtml.cs
@@ -1,13 +1,16 @@
 ﻿using InventoryManagementApplication.DAL;
 using InventoryManagementApplication.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace InventoryManagementApplication.Pages.admin.storage
 {
+	[Authorize(Roles = "Admin")]
 	public class CreateModel : PageModel
 	{
-		private readonly StorageManager _storageManager;
+        
+        private readonly StorageManager _storageManager;
 
 		public CreateModel(StorageManager storageManager)
 		{
@@ -17,10 +20,19 @@ namespace InventoryManagementApplication.Pages.admin.storage
 		[BindProperty]
 		public Storage Storage { get; set; } = default!;
 		public string StatusMessage { get; set; }
+        public string StatusMessage1 { get; set; }
+        public int StorageCount { get; set; }
 
-		public IActionResult OnGet()
+		public async Task<IActionResult> OnGetAsync()
 		{
-			return Page();
+			var storages = await _storageManager.GetStoragesAsync(false);
+			StorageCount = storages.Count();
+
+            if (StorageCount >= 7)
+            {
+                StatusMessage = $"Det finns redan 6 stycken lagerplatser vilket är max! Är du säker på att du vill lägga till fler?";
+            }
+            return Page();
 		}
 
 		public async Task<IActionResult> OnPostAsync()
@@ -30,9 +42,21 @@ namespace InventoryManagementApplication.Pages.admin.storage
 				return Page();
 			}
 
-			await _storageManager.CreateStorageAsync(Storage);
+			var existingStorages = await _storageManager.GetStoragesAsync(false);
 
-			return RedirectToPage("./Index");
+			var existingStorageName = existingStorages.FirstOrDefault(x => x.Name == Storage.Name);
+			StorageCount = existingStorages.Count();
+
+			if(existingStorageName != null)
+			{
+				StatusMessage = "Ett lager med detta namn finns redan. Välj ett annat namn";
+				return Page();
+			}
+
+			await _storageManager.CreateStorageAsync(Storage);
+			StatusMessage1 = $"Du har skapat {Storage.Name} med {Storage.MaxCapacity} platser!";
+
+			return Page();
 		}
 	}
 }
