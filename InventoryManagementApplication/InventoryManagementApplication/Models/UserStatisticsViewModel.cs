2d168fb949b8c26c2e00d0b3debebb74b7074a84
Tintin Falk <tintinfalk01@gmail.com>
2024-10-03 15:14:26 +0200

    Updated Leaderboard

---
+++ InventoryManagementApplication/InventoryManagementApplication/Models/UserStatisticsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoryManagementApplication.Models
+{
+    public class UserStatisticsViewModel
+    {
+        public string? EmployeeNumber { get; set; }
+        public int TotalMovements { get; set; }
+        public int TotalQuantity { get; set; }
+        public List<Statistic> RecentMovements { get; set; } = new List<Statistic>(); 
+    }
+}
