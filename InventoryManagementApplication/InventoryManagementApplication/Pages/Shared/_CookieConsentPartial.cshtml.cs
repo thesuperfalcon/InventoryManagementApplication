06924a126bd8f7e9f34614832202702bbf81858b
Gurra Berg <fnuf_links@hotmail.com>
2024-10-04 10:06:57 +0200

    added cookies

---
+++ InventoryManagementApplication/InventoryManagementApplication/Pages/Shared/_CookieConsentPartial.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InventoryManagementApplication.Pages.Shared
+{
+    public class _CookieConsentPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
