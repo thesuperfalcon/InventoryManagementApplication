b366cb9975282f37ecffb8db119b25c82cd44720
Gurra Berg <fnuf_links@hotmail.com>
2024-10-04 10:33:56 +0200

    dark light mode in progress

---
+++ InventoryManagementApplication/InventoryManagementApplication/wwwroot/js/dark-light-mode.js
@@ -0,0 +1,22 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.getElementById('toggleTheme');
+    const body = document.body;
+
+    // Check local storage for theme preference
+    const theme = localStorage.getItem('theme');
+    if (theme === 'dark') {
+        body.classList.add('dark-mode');
+    }
+
+    toggleButton.addEventListener('click', function () {
+        body.classList.toggle('dark-mode');
+        // Save the user's preference in local storage
+        if (body.classList.contains('dark-mode')) {
+            localStorage.setItem('theme', 'dark');
+            toggleButton.innerHTML = '<i class="bi bi-sun"></i>'; // Change to sun icon
+        } else {
+            localStorage.setItem('theme', 'light');
+            toggleButton.innerHTML = '<i class="bi bi-moon"></i>'; // Change to moon icon
+        }
+    });
+});
