b5afa220eaa3aaaf0daea752e6fac40b98562b04
Gurra Berg <fnuf_links@hotmail.com>
2024-10-17 19:46:21 +0200

    cleanCode sellProduct and javascripts for product intex and sellproduct

--- InventoryManagementApplication/InventoryManagementApplication/Pages/admin/product/SellProduct.cshtml.cs
+++ InventoryManagementApplication/InventoryManagementApplication/Pages/admin/product/SellProduct.cshtml.cs
@@ -30,33 +30,42 @@ namespace InventoryManagementApplication.Pages.admin.product
 
         [BindProperty]
         public int ProductId { get; set; }
-
-        public int Index { get; set; }
-
         [BindProperty]
         public int SellAmount { get; set; }
-        [TempData]
-        public string StatusMessage { get; set; }
+        public int Index { get; set; }
+        public bool IsProductSelected => Product != null && Storage != null;
+
+
         public async Task OnGetAsync(int id, int storageId, int index)
         {
             ProductId = id;
             StorageId = storageId;
             Index = index;
-            if(storageId != 0)
-            {
-                Storage = await _storageManager.GetStorageByIdAsync(StorageId, null);
-            }
-           
-            Product = await _productManager.GetProductByIdAsync(id, null);
-            var getTrackers = await _trackerManager.GetAllTrackersAsync();
-            InventoryTrackers = getTrackers.Where(x => x.ProductId == id).ToList();
-            CurrentProductAmountList = InventoryTrackers.Where(t => t.ProductId == id).Select(x => x.Quantity).ToList();
+
+            await LoadProductsAndStorage(id, storageId);
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(int id, int storageId)
         {
-            Product = await _productManager.GetProductByIdAsync(ProductId, null);
-            Storage = await _storageManager.GetStorageByIdAsync(StorageId, null);
+            ProductId = id;
+            StorageId = storageId;
+
+            if (StorageId == 0)
+            {
+                ModelState.AddModelError(string.Empty, "Du måste välja en produkt innan du säljer.");
+                await LoadProductsAndStorage(id, storageId);
+                return Page();
+            }
+
+            await LoadProductsAndStorage(id, storageId);
+
+            if (Product == null || Storage == null)
+            {
+
+                ModelState.AddModelError(string.Empty, "Du måste välja en giltig produkt och ett lager innan du säljer.");
+                return Page();
+            }
+
             var getTrackers = await _trackerManager.GetAllTrackersAsync();
             InventoryTrackers = getTrackers.Where(x => x.ProductId == Product.Id).ToList();
             int? currentProductAmount = InventoryTrackers.Where(t => t.StorageId == Storage.Id && t.ProductId == Product.Id).Select(x => x.Quantity).FirstOrDefault();
@@ -64,12 +73,12 @@ namespace InventoryManagementApplication.Pages.admin.product
 
             if (SellAmount > currentProductAmount)
             {
-                StatusMessage = "Du kan inte sälja mer produkter än vad som finns i lagret!";
+                TempData["StatusMessageError"] = "Du kan inte sälja mer produkter än vad som finns i lagret!";
                 return RedirectToPage("./SellProduct", new { index = Index, id = Product.Id, storageId = Storage.Id });
             }
             else if(SellAmount <= 0)
             {
-                StatusMessage = "Du kan inte sälje färre än 1 produkt!";
+                TempData["StatusMessageError"] = "Du kan inte sälje färre än 1 produkt!";
                 return RedirectToPage("./SellProduct", new { index = Index, id = Product.Id, storageId = Storage.Id });
             }
             else
@@ -85,11 +94,22 @@ namespace InventoryManagementApplication.Pages.admin.product
                 await _productManager.EditProductAsync(Product);
                 await _trackerManager.EditTrackerAsync(Tracker);
                 await _storageManager.EditStorageAsync(Storage);
-                StatusMessage = "Nu har du sålt " + SellAmount + " antal " + Product.Name;
-                return RedirectToPage("./SellProduct", new { storageId = 0, id = Product.Id });
+                TempData["StatusMessageSuccess"] = $"Nu har du sålt {SellAmount} antal: {Product.Name}";
+                return RedirectToPage("./SellProduct", new { index = Index, id = Product.Id, storageId = Storage.Id });
+            }           
+        }
+
+        private async Task LoadProductsAndStorage(int id, int storageId)
+        {
+            if (storageId != 0)
+            {
+                Storage = await _storageManager.GetStorageByIdAsync(storageId, null);
             }
 
-            
+            Product = await _productManager.GetProductByIdAsync(id, null);
+            var getTrackers = await _trackerManager.GetAllTrackersAsync();
+            InventoryTrackers = getTrackers.Where(x => x.ProductId == id).ToList();
+            CurrentProductAmountList = InventoryTrackers.Where(t => t.ProductId == id).Select(x => x.Quantity).ToList();
         }
     }
 }
